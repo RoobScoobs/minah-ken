0ca2e91cde91c6d9c2fc71948efe0a5c7263fe2b
Ruben <iruben229@gmail.com>
2020-11-13 21:15:03 -0500

    Setting up the database connection to mongodb atlas

---
+++ server/src/database/index.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`
+
+export const connectDatabase = async () => {
+    const client = await MongoClient.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    const db = client.db('main');
+
+    return {
+        recipes: db.collection('recipes')
+    }
+};

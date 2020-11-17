0c1ad6c117a7e1dd5d53a6db4367d788f42c5413
Ruben <iruben229@gmail.com>
2020-11-16 22:30:17 -0500

    Revise the recipe data model

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,12 +1,18 @@
 import { Collection, ObjectId } from 'mongodb';
 
+interface Ingredient {
+    quantity: number;
+    unit: string;
+    description: string;
+}
+
 export interface Recipe {
     _id: ObjectId;
     title: string;
     image: string;
     description: string;
-    ingredients: string;
-    instruction: string;
+    ingredients: Ingredient[];
+    instructions: string[];
 }
 
 export interface Database {
